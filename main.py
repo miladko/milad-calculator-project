fbbb25e1b52e644a720e82a972345f0a9f85f706
Milad <miladko9270@gmail.com>
2025-12-05 17:10:37 +0330

    feat: Add initial user profile variables in Python

---
+++ main.py
@@ -0,0 +1,9 @@
+user_name = 'Milad'
+user_age = 33
+hourly_wage = 10
+is_developer = True
+
+print(f'User Profile:')
+print(f"Name: {user_name}")
+print(f"Age: {user_age}")
+print(f"Is Developer: {is_developer}")
