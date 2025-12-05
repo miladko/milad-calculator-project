9f9fcc138f89b71dfaf22b632805f1be80af6891
Milad <miladko9270@gmail.com>
2025-12-05 17:31:15 +0330

    feat: Add initial user profile in Dart

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  final String userName = 'Milad';
+  final int userAge = 33;
+  final double hourlyWage = 10;
+  final bool isDeveloper = true;
+
+  int currentScore = 10;
+  currentScore = 11;
+
+  print("User Profile:");
+  print("Name: ${userName}");
+  print("Age: ${userAge}");
+  print("Is Developer: ${isDeveloper}");
+  print("Current Score: ${currentScore}");
+}
