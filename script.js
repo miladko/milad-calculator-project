792afdaf4bc20269a16401e2eb8542a954d590ae
Milad <miladko9270@gmail.com>
2025-12-05 17:20:45 +0330

    feat: Add intial user profile in JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const userName = "Milad";
+const userAge = 33;
+const hourlyWage = 10;
+const isDeveloper = true;
+
+let currentScore = 10;
+currentScore = 11;
+
+console.log("User Profile:");
+console.log(`Name: ${userName}`);
+console.log(`Age: ${userAge}`);
+console.log(`Is Developer: ${isDeveloper}`);
+console.log(`Current Score: ${currentScore}`);
