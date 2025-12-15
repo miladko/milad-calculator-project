2878d60a744a4f6cf3d935485fde4b028ac41ef4
Milad <miladko9270@gmail.com>
2025-12-15 17:11:01 +0330

    feat: Complete Super Profile UI with interactivity

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,52 +1,76 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 // The Root of your application
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 233, 234, 235),
+
         // 1. The Top Bar
         appBar: AppBar(
-          title: Text("Super Profile"),
+          title: const Text("Super Profile"),
           backgroundColor: Colors.blueAccent,
         ),
 
-        // 2. The Body (Your work goes here!)
+        // 2. The Body
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.account_circle, size: 100, color: Colors.blue),
-              Text(
+              const Icon(Icons.account_circle, size: 100, color: Colors.blue),
+              const Text(
                 "Milad",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text(
-                "Flutter Developer",
-                style: TextStyle(fontSize: 18, color: Colors.grey),
+                "Ninja Level: $ninjaLevel",
+                style: const TextStyle(fontSize: 18, color: Colors.grey),
               ),
+              const SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-
-                children: [
+                children: const [
                   Icon(Icons.email),
                   SizedBox(width: 20),
                   Icon(Icons.web),
                 ],
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () => print("clicked"),
-                child: Text("Contact me"),
+                onPressed: () {
+                  print("clicked");
+                },
+                child: const Text("Contact me"),
               ),
             ],
           ),
+        ),
+
+        // Floating Action Button
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.grey,
+          onPressed: () {
+            setState(() {
+              ninjaLevel++;
+            });
+          },
+          child: const Icon(Icons.add),
         ),
       ),
     );
